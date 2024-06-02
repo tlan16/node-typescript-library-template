132ca52a0d2039607e956701fae38c71013fd76d
Frank Lan <frank.lan@rea-group.com>
2024-06-02 13:29:54 +1000

    simple fetch

--- lib/main.ts
+++
@@ -1,6 +0,0 @@
-import { logger } from './utilities/logger'
-import { environmentVariables } from './utilities/environmentVariables'
-
-logger.debug(`App started.`, { environmentVariables })
-logger.debug({ environmentVariables })
-logger.info('Hello, world!')
